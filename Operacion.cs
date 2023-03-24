1ab7592e5a98c0796ed650e457f399f8b61930e6
JuanPabloRP <jpabloroldanp@gmail.com>
2023-03-24 14:57:52 -0500

    feat: metodo de multiplicacion listo

---
+++ Operacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nacho_opera
+{
+    public class Operacion
+    {
+        /// <summary>
+        /// Método multiplicacion
+        /// </summary>
+        /// <param name="num1">operando 1</param>
+        /// <param name="num2">operando 2</param>
+        /// <returns></returns>
+        public static double Multiplicacion(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
