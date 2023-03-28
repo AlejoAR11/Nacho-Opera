b1ad3645eb13863b6a1f5d398969449e5d0872fb
SakuSaku <travispastrana075@gmail.com>
2023-03-27 20:24:59 -0500

    Multiplicacion

---
+++ resta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nacho_opera
+{
+    public class Resta
+    {
+        /// <summary>
+        /// Método resta
+        /// </summary>
+        /// <param name="num1">operando 1</param>
+        /// <param name="num2">operando 2</param>
+        /// <returns></returns>
+
+        public static double Divide(double num1, double num2) { 
+            
+            return num1 - num2;
+        
+        }
+    }
+}
